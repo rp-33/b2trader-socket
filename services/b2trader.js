b8339c1eb0fa6f88b8edea209ac09e8d142a352e
Ricardo Perez <rp352429@gmail.com>
2022-12-31 15:00:49 -0400

    change url

--- services/b2trader.js
+++ services/b2trader.js
@@ -1,25 +1,151 @@
 const axios = require('axios');
 
-const cryptocurrency  = {
-    'BTC_USD' : {
+const url = 'https://criptocurrency-files-static.herokuapp.com/images';
+
+export const cryptocurrency = {
+    'BTC_USDT' : {
         'name' : 'Bitcoin',
         'code' : 'BTC',
-        'color' : '#f7931a',
-        'icon' : 'https://res.cloudinary.com/dcds5nieu/image/upload/v1656985058/bitcoin_cav5b2.png'
+        'color' : '#f5b300',
+        'icon' : `${url}/Bitcoin.png`
     },
-    'ETH_USD' : {
+    'ETH_USDT' : {
         'name' : 'Ethereum',
         'code' : 'ETH',
         'color' : '#37367b',
-        'icon' : 'https://res.cloudinary.com/dcds5nieu/image/upload/v1656985169/ethereum_udywml.png'
-    }
+        'icon' : `${url}/Ethereum.png`
+    },
+    'LTC_USDT' : {
+        'name' : 'Litecoin',
+        'code' : 'LTC',
+        'color' : '#bfbfbf',
+        'icon' : `${url}/Litecoin.jpg`
+    },
+    'MATIC_USDT' : {
+        'name' : 'Matic',
+        'code' : 'MATIC',
+        'color' : '#37367b',
+        'icon' : `${url}/Litecoin.jpg`
+    },
+    'UNI_USDT' : {
+        'name' : 'Uniswap',
+        'code' : 'UNI',
+        'color' : '#fd007a',
+        'icon' : `${url}/Uniswap.png`
+    },
+    'DASH_USDT' : {
+        'name' : 'Dash',
+        'code' : 'DASH',
+        'color' : '#138de4',
+        'icon' : `${url}/Dash.png`
+    },
+    'TRX_USDT' : {
+        'name' : 'Tron',
+        'code' : 'TRX',
+        'color' : '#cb4a42',
+        'icon' : `${url}/Tron.png`
+    },
+    'BNB_USDT' : {
+        'name' : 'Binance coin',
+        'code' : 'BNB',
+        'color' : '#f3bb32',
+        'icon' : `${url}/Binance.png`
+    },
+    'DOGE_USDT' : {
+        'name' : 'Doge coin',
+        'code' : 'DOGE',
+        'color' : '#ba9f33',
+        'icon' : `${url}/Dogecoin.png`
+    },
+    'XMR_USDT' : {
+        'name' : 'Monero',
+        'code' : 'XMR',
+        'color' : '#fd6b03',
+        'icon' : `${url}/Monero.png`
+    },
+    'AAVE_USDT' : {
+        'name' : 'Aave',
+        'code' : 'AAVE',
+        'color' : '#37367b',
+        'icon' : `${url}/Litecoin.jpg`
+    },
+    'SUSHI_USDT' : {
+        'name' : 'Sushi',
+        'code' : 'SUSHI',
+        'color' : '#b86eba',
+        'icon' : `${url}/SushiSwap.png`
+    },
+    'XRP_USDT' : {
+        'name' : 'Ripple',
+        'code' : 'XRP',
+        'color' : '#37367b',
+        'icon' : `${url}/Litecoin.jpg`
+    },
+    'BCH_USDT' : {
+        'name' : 'Bitcoin cash',
+        'code' : 'BCH',
+        'color' : '#4bce51',
+        'icon' : `${url}/BitcoinCash.png`
+    },
+    'XLM_USDT' : {
+        'name' : 'Stellar',
+        'code' : 'XLM',
+        'color' : '#303030',
+        'icon' : `${url}/Stellar.png`
+    },
+    'LINK_USDT' : {
+        'name' : 'Chainlink',
+        'code' : 'LINK',
+        'color' : '#375bd2',
+        'icon' : `${url}/Chainlink.png`
+    },
+    'ZEC_USDT' : {
+        'name' : 'Zcash',
+        'code' : 'ZEC',
+        'color' : '#dea038',
+        'icon' : `${url}/ZCash.png`
+    },
+    'BAT_USDT' : {
+        'name' : 'Basic attention token',
+        'code' : 'BAT',
+        'color' : '#df4729',
+        'icon' : `${url}/BasicAttentionToken.png`
+    },
+    'MANA_USDT' : {
+        'name' : 'Mana',
+        'code' : 'MANA',
+        'color' : '#fd5e56',
+        'icon' : `${url}/mana.png`
+    },
 }
+
+export const cryptoAvailable = [
+    'BTC_USDT',
+    'ETH_USDT',
+    'LTC_USDT',
+    'MATIC_USDT',
+    'UNI_USDT',
+    'DASH_USDT',
+    'TRX_USDT',
+    'BNB_USDT',
+    'DOGE_USDT',
+    'XMR_USDT',
+    'AAVE_USDT',
+    'SUSHI_USDT',
+    'XRP_USDT',
+    'BCH_USDT',
+    'XLM_USDT',
+    'LINK_USDT',
+    'ZEC_USDT',
+    'BAT_USDT',
+    'MANA_USDT'
+];
 
 const cryptoAvailable = ['BTC_USD','ETH_USD'];
 
 class B2trader {
     constructor(io){
-        this.furl =  `https://b2t-api-cmc-staging-5.flexprotect.org/marketdata/cmc/v1/summary`;
+        this.furl =  `https://b2t-api-cashship.flexprotect.org/marketdata/cmc/v1/summary`;
     }
 
     async getSummary(){
